7819ff368da17c0ba4f3f960395b37861542cc3b
Marcelo García <43250973+chelogithub@users.noreply.github.com>
2024-06-09 23:49:27 -0300

    RTC_HandleTypeDef

--- BKP_REG.c
+++ BKP_REG.c
@@ -6,25 +6,26 @@
 *
 *******************************************************************************/
 #include "BKP_REG.h"
+#include "main.h"
 
-int BKP_REG_blk(uint8_t WR, uint8_t regs, uint32_t * data)
+int BKP_REG_blk(RTC_HandleTypeDef *RTCreg, uint8_t WR, uint8_t regs, uint32_t * data)
 {
-		  if (WR==1) HAL_PWR_EnableBkUpAccess();	//Habilito el acceso de escriura a los registros
+		  if (WR == 1) HAL_PWR_EnableBkUpAccess();	//Habilito el acceso de escriura a los registros
 
-		  if (regs< 20)
+		  if (regs < 20)
 		  {
-			for(int i=0; i<=regs-1; i++)
+			for(int i=0; i<= (regs-1); i++)
 				{
 					if (WR==0)
-					{ HAL_RTCEx_BKUPWrite(&hrtc, i, data[i]); //Escritura en los registros
+					{ HAL_RTCEx_BKUPWrite(RTCreg, i, data[i]); //Escritura en los registros
 					}
 					else
-					{ data[i]=HAL_RTCEx_BKUPRead(&hrtc, i);	//Lectura desde los registros
+					{ data[i]=HAL_RTCEx_BKUPRead(RTCreg, i);	//Lectura desde los registros
 					}
 				}
-			if (WR==1)	HAL_PWR_DisableBkUpAccess();	//Deshabilito el acceso de escriura a los registros
+			if (WR == 1)	HAL_PWR_DisableBkUpAccess();	//Deshabilito el acceso de escriura a los registros
 			return(1);
 		  }
 		  else
-		  	{return 0}
+		  	{return (0);}
 }
--- BKP_REG.h
+++ BKP_REG.h
@@ -7,12 +7,12 @@
 ******************************************************************************/
 #include "main.h"
 
-uint8_t BKP_REG_blk(uint8_t , uint8_t, uint32_t * )
+int BKP_REG_blk(RTC_HandleTypeDef *, uint8_t , uint8_t, uint32_t * );
 
 /****************************************************************
 *	Lee o escribe los registros de BackUp del RTC    	  		*
 *																*
-*																*
+*	RTC_HandleTypeDef hrtc															*
 *	uint8_t		:	0 = Error  1=OK					    		*
 *	uint8_t	    :	0 = Escritura  1=Lectura					*
 *	uint8_t		:	Cantidad de registros a aceder  			*
