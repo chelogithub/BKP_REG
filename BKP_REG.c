4c4b8145c7f95ad90f5dbadf35eb942b44188cbd
Marcelo García <43250973+chelogithub@users.noreply.github.com>
2024-06-11 00:37:14 -0300

    feat: function to save IP as int in BKP_REGS
    no error processing, no optimizing, only test.

--- BKP_REG.c
+++ BKP_REG.c
@@ -32,5 +32,89 @@ int BKP_REG_blk(RTC_HandleTypeDef *RTCreg, uint8_t WR, uint8_t regs, uint32_t * 
 
 int BKP_RG_IP(RTC_HandleTypeDef *RTCreg, uint8_t WR, uint8_t addr, char * data)
 {
+	int lng_data=0,i=0,io=0,nocteto=1;
+	uint32_t BKP_REG=0;
+	uint32_t octd,octe2,octe3,octe4;
+	char octc[4],oct2[4],oct3[4],oct4[4];
+
+	lng_data=strlen(data);
+
+
+	octd=0;
+	while(*data != '.' )
+	{
+		octc[io]=*data;
+		*data++;
+		io++;
+		i++;
+		if(i==4) return(1); //Error en longitud de caracteres
+	}
+	*data++;
+	i++;
+	octc[io]='\0';
+	octd=atoi(octc);
+	BKP_REG=octd;
+
+	io=0;
+	while(*data != '.' )
+	{
+		octc[io]=*data;
+		*data++;
+		io++;
+		i++;
+		if(i==4) return(1); //Error en longitud de caracteres
+	}
+	*data++;
+	i++;
+	octc[io]='\0';
+	octd=atoi(octc);
+
+
+	BKP_REG=BKP_REG<<8;
+	BKP_REG=BKP_REG | octd;
+
+	io=0;
+	while(*data != '.' )
+	{
+		octc[io]=*data;
+		*data++;
+		io++;
+		i++;
+		if(i==4) return(1); //Error en longitud de caracteres
+	}
+	*data++;
+	i++;
+	octc[io]='\0';
+	octd=atoi(octc);
+
+
+	BKP_REG=BKP_REG<<8;
+	BKP_REG=BKP_REG | octd;
+
+	io=0;
+	while(*data != '\0' )
+	{
+		octc[io]=*data;
+		*data++;
+		io++;
+		i++;
+		if(i==4) return(1); //Error en longitud de caracteres
+	}
+	*data++;
+	i++;
+	octc[io]='\0';
+	octd=atoi(octc);
+
+
+	BKP_REG=BKP_REG<<8;
+	BKP_REG=BKP_REG | octd;
+
+	if(WR==1)
+	{
+		HAL_PWR_EnableBkUpAccess();
+		HAL_RTCEx_BKUPWrite(RTCreg, addr, BKP_REG);
+		HAL_PWR_DisableBkUpAccess();
+
+	}
 
 }
