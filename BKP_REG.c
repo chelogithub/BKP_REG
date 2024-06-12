37849da8216f2705a3b5759c30b18a3a277d487e
Marcelo García <43250973+chelogithub@users.noreply.github.com>
2024-06-11 23:16:57 -0300

    feat: add read ip, write  2 x uint16_t, read 2 x uint16_t

--- BKP_REG.c
+++ BKP_REG.c
@@ -39,82 +39,151 @@ int BKP_RG_IP(RTC_HandleTypeDef *RTCreg, uint8_t WR, uint8_t addr, char * data)
 
 	lng_data=strlen(data);
 
+    if(WR==1)
+    {
+    	octd=0;
+    	while(*data != '.' )
+    	{
+    		octc[io]=*data;
+    		*data++;
+    		io++;
+    		i++;
+    		if(i==4) return(1); //Error en longitud de caracteres
+    	}
+    	*data++;
+    	i++;
+    	octc[io]='\0';
+    	octd=atoi(octc);
+    	BKP_REG=octd;
 
-	octd=0;
-	while(*data != '.' )
-	{
-		octc[io]=*data;
-		*data++;
-		io++;
-		i++;
-		if(i==4) return(1); //Error en longitud de caracteres
-	}
-	*data++;
-	i++;
-	octc[io]='\0';
-	octd=atoi(octc);
-	BKP_REG=octd;
-
-	io=0;
-	while(*data != '.' )
-	{
-		octc[io]=*data;
-		*data++;
-		io++;
-		i++;
-		if(i==4) return(1); //Error en longitud de caracteres
-	}
-	*data++;
-	i++;
-	octc[io]='\0';
-	octd=atoi(octc);
-
-
-	BKP_REG=BKP_REG<<8;
-	BKP_REG=BKP_REG | octd;
-
-	io=0;
-	while(*data != '.' )
-	{
-		octc[io]=*data;
-		*data++;
-		io++;
-		i++;
-		if(i==4) return(1); //Error en longitud de caracteres
-	}
-	*data++;
-	i++;
-	octc[io]='\0';
-	octd=atoi(octc);
+    	io=0;
+    	while(*data != '.' )
+    	{
+    		octc[io]=*data;
+    		*data++;
+    		io++;
+    		i++;
+    		if(i==4) return(1); //Error en longitud de caracteres
+    	}
+    	*data++;
+    	i++;
+    	octc[io]='\0';
+    	octd=atoi(octc);
 
 
-	BKP_REG=BKP_REG<<8;
-	BKP_REG=BKP_REG | octd;
+    	BKP_REG=BKP_REG<<8;
+    	BKP_REG=BKP_REG | octd;
 
-	io=0;
-	while(*data != '\0' )
-	{
-		octc[io]=*data;
-		*data++;
-		io++;
-		i++;
-		if(i==4) return(1); //Error en longitud de caracteres
-	}
-	*data++;
-	i++;
-	octc[io]='\0';
-	octd=atoi(octc);
+    	io=0;
+    	while(*data != '.' )
+    	{
+    		octc[io]=*data;
+    		*data++;
+    		io++;
+    		i++;
+    		if(i==4) return(1); //Error en longitud de caracteres
+    	}
+    	*data++;
+    	i++;
+    	octc[io]='\0';
+    	octd=atoi(octc);
 
 
-	BKP_REG=BKP_REG<<8;
-	BKP_REG=BKP_REG | octd;
+    	BKP_REG=BKP_REG<<8;
+    	BKP_REG=BKP_REG | octd;
 
-	if(WR==1)
-	{
+    	io=0;
+    	while(*data != '\0' )
+    	{
+    		octc[io]=*data;
+    		*data++;
+    		io++;
+    		i++;
+    		if(i==4) return(1); //Error en longitud de caracteres
+    	}
+    	*data++;
+    	i++;
+    	octc[io]='\0';
+    	octd=atoi(octc);
+
+
+    	BKP_REG=BKP_REG<<8;
+    	BKP_REG=BKP_REG | octd;
+
 		HAL_PWR_EnableBkUpAccess();
 		HAL_RTCEx_BKUPWrite(RTCreg, addr, BKP_REG);
 		HAL_PWR_DisableBkUpAccess();
 
-	}
+    }
+    else if(WR==0)
+		{
+		char returnVector[15];
+		returnVector[0]='\0';
+		uint32_t shifts=0;
 
+		BKP_REG=HAL_RTCEx_BKUPRead(RTCreg, addr);	//Leo el dato en memoria de 32bits
+
+		shifts=BKP_REG;
+		octd=(shifts>>24)&0x000000FF;
+		INTOA(octd, octc);		//Se pasa el entero a char y se guarda en octc.
+
+		strncat(data,octc,strlen(octc));
+		strncat(data,".",1);
+
+		shifts=BKP_REG;
+		octd=(shifts>>16)&0x000000FF;
+		INTOA(octd, octc);		//Se pasa el entero a char y se guarda en octc.
+
+		strncat(data,octc,strlen(octc));
+		strncat(data,".",1);
+
+		shifts=BKP_REG;
+		octd=(shifts>>8)&0x000000FF;
+		INTOA(octd, octc);		//Se pasa el entero a char y se guarda en octc.
+
+		strncat(data,octc,strlen(octc));
+		strncat(data,".",1);
+
+
+		shifts=BKP_REG;
+		octd=shifts&0x000000FF;
+		INTOA(octd, octc);		//Se pasa el entero a char y se guarda en octc.
+
+		strncat(data,octc,strlen(octc));
+		}
+
+
+
+}
+
+int BKP_RG_2int(RTC_HandleTypeDef *RTCreg, uint8_t WR, uint8_t addr, char * data,char * data2)
+{
+	uint32_t num,num2=0;
+
+    if(WR==1)
+    {
+    	num=atoi(data);
+    	if (num<65536)
+    	{
+    		num=num<<16;
+    	}
+    	num2=atoi(data2);
+    	if (num2<65536)
+    	{
+    		num=num|(num2&0x0000FFFF);
+    	}
+
+		HAL_PWR_EnableBkUpAccess();
+		HAL_RTCEx_BKUPWrite(RTCreg, addr, num);
+		HAL_PWR_DisableBkUpAccess();
+
+    }
+    else if(WR==0)
+		{
+			num=HAL_RTCEx_BKUPRead(RTCreg, addr);
+			num2=((num>>16)&0x0000FFFF);
+			INTOA(num2, data);
+			num2=(num & 0x0000FFFF);
+			INTOA(num2,data2);
+		}
 }
--- BKP_REG.h
+++ BKP_REG.h
@@ -18,7 +18,7 @@ int BKP_REG_blk(RTC_HandleTypeDef *, uint8_t , uint8_t, uint32_t * );
 /****************************************************************
 *	Lee o escribe los registros de BackUp del RTC    	  		*
 *																*
-*	RTC_HandleTypeDef hrtc															*
+*	RTC_HandleTypeDef hrtc										*
 *	uint8_t		:	0 = Error  1=OK					    		*
 *	uint8_t	    :	0 = Escritura  1=Lectura					*
 *	uint8_t		:	Cantidad de registros a acceder  			*
@@ -26,7 +26,7 @@ int BKP_REG_blk(RTC_HandleTypeDef *, uint8_t , uint8_t, uint32_t * );
 *																*
 ****************************************************************/
 
-int BKP_RG_IP(RTC_HandleTypeDef *RTCreg, uint8_t WR, uint8_t addr, char * data);
+int BKP_RG_IP(RTC_HandleTypeDef *, uint8_t , uint8_t , char * );
 
 /****************************************************************
 *	Lee o escribe datos de IP en     	  						*
@@ -38,3 +38,15 @@ int BKP_RG_IP(RTC_HandleTypeDef *RTCreg, uint8_t WR, uint8_t addr, char * data);
 *																*
 ****************************************************************/
 
+int BKP_RG_2int(RTC_HandleTypeDef *, uint8_t , uint8_t , char * ,char * );
+
+/****************************************************************
+*	Lee o escribe dos enteros de 16 bitts en nuna de 32			*
+*																*
+*	RTC_HandleTypeDef hrtc										*
+*	uint8_t		:	1 = Escritura  0=Lectura			  		*
+*	uint8_t	    :	Dirección a la que se quiere acceder		*
+*	char *		:	Vector que entrega o recibe los datos  		*
+*	char *		:	Vector que entrega o recibe los datos  		*
+*																*
+****************************************************************/
